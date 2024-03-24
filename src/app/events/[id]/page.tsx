a51dad31cfaadb18c2a7ee35b0216cbf13dc4e0d
danielcaldeir <danielcaldeir@gmail.com>
2024-03-24 16:34:44 -0300

    Finalizacao do Projeto

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,14 +1,57 @@
+import { getEvent } from "@/api/site";
+import { Search } from "@/components/site/Search";
+import { 
+    Card, 
+    CardContent, 
+    CardDescription, 
+    CardFooter, 
+    CardHeader, 
+    CardTitle 
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+
 type Props = {
     params: {
         id: string;
     } 
 }
 
-const Page = ({params}: Props) => {
+const Page = async ({params}: Props) => {
+    const idEvent = parseInt(params.id);
+    const eventItem = await getEvent(idEvent);
+    if (!eventItem) return redirect('/');
+    if (!eventItem.status) return redirect('/');
+
     return (
-        <div>
-            ID do Evento: {params.id}
-        </div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-20">
+            <Card className="flex items-center justify-between ">
+                <CardHeader className="outline-none">
+                    <Link href="/">
+                        <h1 className="text-xl text-red-200 font-semibold text-center">
+                            <span >Amigo</span> Secreto
+                        </h1>
+                    </Link>
+                    <CardTitle>{eventItem.title}</CardTitle>
+                    <CardDescription>{eventItem.description}</CardDescription>
+                </CardHeader>
+                
+                
+            </Card>
+            <Card>
+                
+                    {/* <p>Home - Amigo Secreto</p> */}
+                    <Search id={eventItem.id}/>
+                
+            </Card>
+            <Card>
+                <CardFooter>
+                    <Label>Criado por Daniel Caldeira</Label>
+                </CardFooter>
+            </Card>
+        </main>
     );
 }
 
